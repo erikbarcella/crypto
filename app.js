6d7734f16818ca3d8dadee05abc1253bf9c436ab
Erik Barcella <erikbarcellatrisch@gmail.com>
2022-11-11 11:21:33 -0300

    arquivos iniciais do projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+})
+
+let port = 3000;
+app.listen(port, () =>{
+    console.log("Server running on port: "+ port);
+})
