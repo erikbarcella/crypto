6b22bbeb0635abf3e90666edee37b880b71017e9
Erik Barcella <erikbarcellatrisch@gmail.com>
2022-11-23 17:54:49 -0300

    implatanção api back end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
+const request = require('request');
 
 
 app.set('view engine', 'ejs');
@@ -9,13 +10,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req,res)=>{
-    res.render("index.ejs");
+let resposta = {};
+
+app.get('/', (req,res)=>{
+    res.render("index");
 })
 
-app.get("/cotacao", (req,res)=>{
-    
-})
+app.get('/cotacao', async(req,res)=>{
+    const{selectOne,selectTwo } = req.query;
+
+   await request(`https://economia.awesomeapi.com.br/last/${selectOne+"-"+selectTwo}`, (error,response,body)=>{
+
+        if(!error && response.statusCode==200){
+            resposta = JSON.parse(body);
+            console.log(resposta);
+        }
+        res.render("resultado", {resposta});
+    })
+
+    }
+)
 
 let port = 3000;
 app.listen(port, () =>{
