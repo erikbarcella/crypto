2b8322360742fb8988a7d6b6774e24c1290a6f66
Erik Barcella <agenciaebdigital@gmail.com>
2022-11-24 21:44:00 -0300

    implementado conversoes globais

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require ('path');
 const request = require('request');
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,18 +13,12 @@ let resposta = {};
 let ret = [""];
 let values;
 
-app.get('/', (req,res)=>{
-    res.render("index");
-})
+app.get('/', async(req,res)=>{
 
-app.get('/cotacao', async(req,res)=>{
-    const{selectOne,selectTwo } = req.query;
-
-    await request(`https://economia.awesomeapi.com.br/last/${selectOne+"-"+selectTwo}`, (error,response,body)=>{
+    await request("https://economia.awesomeapi.com.br/last/USD-BRL", (error,response,body)=>{
 
         if(!error && response.statusCode==200){
             resposta = JSON.parse(body);
-
             ret[1] = resposta;
             let arr = ret.map(function(obj){
                 return Object.keys(obj).map(function(key){
@@ -34,7 +27,28 @@ app.get('/cotacao', async(req,res)=>{
             })
             values = arr[1].find(element => element)
         }
-        res.redirect("resultado", {values});
+        res.render('index', {values});
+    })
+})
+        
+   
+
+app.get('/cotacao', async(req,res)=>{
+    const{selectOne,selectTwo,quantidade } = req.query;
+
+    await request(`https://economia.awesomeapi.com.br/last/${selectOne+"-"+selectTwo}`, (error,response,body)=>{
+
+        if(!error && response.statusCode==200){
+            resposta = JSON.parse(body);
+            ret[1] = resposta;
+            let arr = ret.map(function(obj){
+                return Object.keys(obj).map(function(key){
+                    return obj[key] 
+                })
+            })
+            values = arr[1].find(element => element)
+        }
+        res.render('resultado', {values,quantidade,selectTwo,selectOne});
     })
     }
 )
